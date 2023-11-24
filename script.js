cce8b0c0a07d476508369c780abf9ad3bb255e06
bello abdul samed <blayonstance@blayonstance.bello.com>
2023-11-24 10:04:34 -0600

    general progress made:(

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-document.querySelectorAll('section p').forEach(function(paragraph) {
-    paragraph.addEventListener('click', function() {
-        alert('You clicked on a paragraph!');
-    });
-});
+
+
+let currentSlide = 0;
+
+function nextSlide() {
+  if (currentSlide < 3) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  updateSlider();
+}
+
+function prevSlide() {
+  if (currentSlide > 0) {
+    currentSlide--;
+  } else {
+    currentSlide = 3;
+  }
+  updateSlider();
+}
+updateSlider();
+function updateSlider() {
+    const slider = document.querySelector('.slider');
+    const slideWidth = document.querySelector('.slider').offsetWidth;
+    const newPosition = -currentSlide * (slideWidth + 20); // Adjusted for margin
+    slider.style.transform = `translateX(${newPosition}px)`;
+  
+    // Update button visibility based on current slide
+    document.querySelector('.prev').style.display = currentSlide === 0 ? 'none' : 'block';
+    document.querySelector('.next').style.display = currentSlide === 3 ? 'none' : 'block';
+  }
+  
