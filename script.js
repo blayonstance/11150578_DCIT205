043ddc68baddac06f7a898630afc8c38e42bcc56
bello abdul samed <blayonstance@blayonstance.bello.com>
2023-11-26 09:41:17 -0600

    add few cahnges to some buttons and finished event page

--- script.js
+++ script.js
@@ -23,10 +23,9 @@ updateSlider();
 function updateSlider() {
     const slider = document.querySelector('.slider');
     const slideWidth = document.querySelector('.slider').offsetWidth;
-    const newPosition = -currentSlide * (slideWidth + 20); // Adjusted for margin
+    const newPosition = -currentSlide * (slideWidth + 20); 
     slider.style.transform = `translateX(${newPosition}px)`;
   
-    // Update button visibility based on current slide
     document.querySelector('.prev').style.display = currentSlide === 0 ? 'none' : 'block';
     document.querySelector('.next').style.display = currentSlide === 3 ? 'none' : 'block';
   }
